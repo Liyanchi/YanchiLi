b23cf7bc0178c6cdf8c04b8750e1281c46225fd9
JINGNILItina <jli239@ucsc.edu>
2016-01-22 21:30:57 -0800

    294hw2

---
+++ YanchiLiAssignment2Creator.R
@@ -0,0 +1,42 @@
+##0##
+YanchiLiAssignment2<-list(firstName="Yanchi",lastName="Li",email="yli248@ucsc.edu",studentID = 1505113)
+
+
+##1##
+install.packages("repmis")
+library("repmis")
+Diamonds <- source_data("https://raw.githubusercontent.com/EconomiCurtis/econ294_2015/master/data/diamonds.CSV")
+YanchiLiAssignment2$s1a<-nrow(Diamonds)
+YanchiLiAssignment2$s1b<-ncol(Diamonds)
+YanchiLiAssignment2$s1c<-names(Diamonds)
+YanchiLiAssignment2$s1d<-summary(Diamonds$price)
+
+###2##
+dftd <- read.table("https://github.com/EconomiCurtis/econ294_2015/raw/master/data/NHIS_2007_TSV.txt",header=T)
+YanchiLiAssignment2$s2a<-nrow(dftd)
+YanchiLiAssignment2$s2b<-ncol(dftd)
+YanchiLiAssignment2$s2c<-names(dftd)
+YanchiLiAssignment2$s2d<-mean(dftd$weight)
+YanchiLiAssignment2$s2e<-median(dftd$weight)
+dftd$weight2<-ifelse(dftd$weight>999|dftd$weight<996,dftd$weight,0)
+hist(dftd$weight2)
+table(dftd$weight2)
+YanchiLiAssignment2$s2f<-mean(dftd$weight2)
+YanchiLiAssignment2$s2g<-median(dftd$weight2)
+female<-subset(dftd,sex=2)
+YanchiLiAssignment2$s2h<-summary(female$weight)
+male<-subset(dftd,sex=1)
+YanchiLiAssignment2$s2i<-summary(male$weight)
+
+###3##
+vec<-c(letters,LETTERS)
+YanchiLiAssignment2$s3a<-paste(vec[c(1:26*2)])
+YanchiLiAssignment2$s3b<-paste(vec[c(36,1,14)])
+arr<-array(c(letters,LETTERS),dim=c(3,3,3))
+View(arr)
+arr1<-arr[,,1]
+arr2<-arr[,,2]
+arr3<-arr[,,3]
+YanchiLiAssignment2$s3c<-arr2[,1]
+YanchiLiAssignment2$s3d<-c(arr1[2,2],arr2[2,2],arr3[2,2])
+YanchiLiAssignment2$s3e<-paste(arr[1,3,3],arr[1,1,1],arr[2,2,2])
